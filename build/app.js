09303284838c5c7e285d52e5ee88de41502bbb35
Brian Chu <brianchu@D4331.NET>
2017-05-19 10:08:48 +0800

    init

--- build/app.js
+++ build/app.js
@@ -1,5 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+Date.prototype.yyyyMMddHHmmss = function () {
+    var date = this;
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var hh = date.getHours();
+    var mm = date.getMinutes();
+    var ss = date.getSeconds();
+    return "" + year +
+        (month < 10 ? "0" + month : month) +
+        (day < 10 ? "0" + day : day) +
+        (hh < 10 ? "0" + hh : hh) +
+        (mm < 10 ? "0" + mm : mm) +
+        (ss < 10 ? "0" + ss : ss);
+};
 var mqtt = require("mqtt");
 var client = mqtt.connect("wss://mbltest01.mqtt.iot.gz.baidubce.com:8884/mqtt", {
     username: "mbltest01/eight",
@@ -8,25 +23,29 @@ var client = mqtt.connect("wss://mbltest01.mqtt.iot.gz.baidubce.com:8884/mqtt", 
 client.on("connect", function (connack) {
     console.log("on connect");
     console.log(JSON.stringify(connack));
-    client.subscribe("eight/#", function (err, granted) {
+    client.subscribe("toEight/#", function (err, granted) {
         console.log("subscribe");
         console.log(JSON.stringify(err));
         console.log(JSON.stringify(granted));
-        if ((typeof err === "undefined" || err === null) && granted.some(function (value) { return value.topic === "eight/#" && value.qos !== 128; })) {
+        if ((typeof err === "undefined" || err === null) && granted.some(function (value) { return value.topic === "toEight/#" && value.qos !== 128; })) {
             client.on("message", function (topic, message, packet) {
                 console.log("on message");
                 console.log(JSON.stringify(topic));
                 console.log(JSON.stringify(message));
                 console.log(JSON.stringify(packet));
                 console.log(topic + ": " + message.toString());
+                var jsonMessage = JSON.parse(message.toString());
                 switch (topic) {
-                    case "eight/i.am":
+                    case "toEight/i.am":
                         var data = {
-                            who: "eight",
-                            what: "body",
+                            who: jsonMessage.whoAmI,
                             when: new Date().yyyyMMddHHmmss()
                         };
-                        client.publish("eight/i.am", JSON.stringify(data), function (err) {
+                        client.publish("body/you.are", JSON.stringify(data), function (err) {
+                            console.log("publish");
+                            console.log(JSON.stringify(err));
+                        });
+                        client.publish("fromEight/he.is", JSON.stringify(data), function (err) {
                             console.log("publish");
                             console.log(JSON.stringify(err));
                         });
